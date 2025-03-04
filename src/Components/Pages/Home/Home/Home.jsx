dca40561f8ce0d3e49188f8cde79f3e63d7ae4a9
BD-Arfat <arfat1islam5162@gmail.com>
2025-03-03 18:55:52 -0700

    wedding section done

--- src/Components/Pages/Home/Home/Home.jsx
+++ src/Components/Pages/Home/Home/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import Banner from '../Banner/Banner';
 import BrandName from '../BrandName/BrandName';
+import WeddingStyleGuide from '../WeddingStyleGuide/WeddingStyleGuide';
 
 const Home = () => {
     return (
         <div>
             <Banner></Banner>
             <BrandName></BrandName>
+            <WeddingStyleGuide></WeddingStyleGuide>
         </div>
     );
 };
